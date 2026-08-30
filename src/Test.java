8250af1047b8d6e9f82cf41e765b4d952ed83eb6
currymike123 <currymike@gmail.com>
2026-08-30 17:44:07 -0400

    Fix test

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,6 @@
 public class Test {
-    public static void main(String[] args)
-    {
-        int myNumber=5
-        System.out.println("Testing " + myNumber)
+    public static void main(String[] args) {
+        int myNumber = 5;
+        System.out.println("Testing " + myNumber);
     }
 }
